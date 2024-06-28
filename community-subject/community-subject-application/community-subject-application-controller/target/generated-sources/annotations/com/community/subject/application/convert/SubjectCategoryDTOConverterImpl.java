17248e455a95312411a8b8a85b2c83af4fbbc18e
weixiaoyan132 <1326478219@qq.com>
2024-06-28 19:25:34 +0800

    修改lombok版本

--- community-subject/community-subject-application/community-subject-application-controller/target/generated-sources/annotations/com/community/subject/application/convert/SubjectCategoryDTOConverterImpl.java
+++ community-subject/community-subject-application/community-subject-application-controller/target/generated-sources/annotations/com/community/subject/application/convert/SubjectCategoryDTOConverterImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-28T16:41:22+0800",
+    date = "2024-06-28T19:24:00+0800",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_341 (Oracle Corporation)"
 )
 public class SubjectCategoryDTOConverterImpl implements SubjectCategoryDTOConverter {
--- community-subject/community-subject-domain/target/generated-sources/annotations/com/community/subject/domain/convert/SubjectAnswerConverterImpl.java
+++ community-subject/community-subject-domain/target/generated-sources/annotations/com/community/subject/domain/convert/SubjectAnswerConverterImpl.java
@@ -10,7 +10,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-28T16:41:18+0800",
+    date = "2024-06-28T19:23:57+0800",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_341 (Oracle Corporation)"
 )
 public class SubjectAnswerConverterImpl implements SubjectAnswerConverter {
